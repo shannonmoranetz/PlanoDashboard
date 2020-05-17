f68b85fd436f80faf9dbe443d7a10a5fdb5f97c5
shannonmoranetz <shannonrmoranetz@gmail.com>
2020-05-16 18:21:10 -0600

    Add initial components and service files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { DataTableComponent } from './components/data-table/data-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    DataTableComponent
   ],
   imports: [
     BrowserModule,
